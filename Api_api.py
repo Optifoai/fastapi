c446dbe520e33327871d38b38effc2b51160eb7e
pradyumna <pradyumnau99@gmail.com>
2025-11-24 00:31:50 +0530

    add remove bg code

--- Api_api.py
+++ Api_api.py
@@ -11,6 +11,12 @@ from io import BytesIO
 from fastapi.responses import StreamingResponse
 from typing import List
 from ai_video.main_video import process_car_reel
+import shutil
+import requests
+import uuid
+from rembg import remove
+from PIL import Image
+import io
 
 app = FastAPI()
 
@@ -173,6 +179,22 @@ async def generate_reel(car_request: CarRequest):
         
     except Exception as e:
        return Response(content=str(e), status_code=500)
+
+@app.post("/remove-background/")
+async def remove_background(file: UploadFile = File(...)):
+    # Read the uploaded image
+    input_image = Image.open(file.file)
+
+    # Remove the background (output will have transparent background)
+    output_image = remove(input_image)
+
+    # Save the output image with transparent background to a bytes buffer
+    output_buffer = io.BytesIO()
+    output_image.save(output_buffer, format="PNG")  # Save as PNG to preserve transparency
+    output_buffer.seek(0)  # Move the buffer's cursor to the beginning
+
+    # Return the image as a response with transparent background
+    return Response(content=output_buffer.getvalue(), media_type="image/png")
 
 # Run FastAPI
 if __name__ == "__main__":
